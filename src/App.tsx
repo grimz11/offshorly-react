233f187c944acc8ce419913563d16741b9579159
Mark Andog <andogreymark@gmail.com>
2022-03-28 01:17:25 +0800

    fix: remove query dev tools

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ function App() {
           <HomeScene />
         </ThemeProvider>
       </AppDataProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
